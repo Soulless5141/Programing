c3c4795560c06d07ec0b547c07705b73f98e30ba
yoshiki-nakama <104492110+yoshiki-nakama@users.noreply.github.com>
2024-02-14 10:43:07 +0900

    爆発画像を入れる変数を追加

--- Game/DriveAndAvoid/Scene/GameMainScene.h
+++ Game/DriveAndAvoid/Scene/GameMainScene.h
@@ -16,6 +16,7 @@ private:
 	int mileage;           //走行距離
 	int enemy_image[3];    //敵画像
 	int enemy_count[3];    //通り過ぎた敵カウント
+	int explosionimg;	   //爆破画像 
 	Player* player;        //プレイヤー
 	Enemy** enemy;         //敵
 	std::vector<Item> item;//アイテム
